e43b13d1bc47e50d9e7bc2d08b156edba1ce1102
Arshdeep singh <arshgill6120@gmail.com>
2025-08-21 16:04:32 +0530

    learned error handeling

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,38 @@ import "../data/backend-practice.js";
 import { loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
+// async -- a better way to work with promises ; makes a function return a promise.
+
+async function loadPage() {
+  try {
+    console.log("load page");
+    await loadProductsFetch();
+    await new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+    });
+  } catch (error) {
+    console.log("Unexpected Error. Please try again later.");
+  }
+
+  // throw does not work in the future.
+  renderOrderSummary();
+  renderPaymentSummary();
+  // returning in this function will result in automatic wrap of that statement, that word, that string with a resolve function.
+}
+
+// await -- lets us write asynchronous code like normal code.
+// We can only use await when we're inside an async function.
+// async await can only be used with promises, it doesn't do anything with a callback.
+
+// the closest function will have to be async.
+
+// use async await over promises and callbacks
+loadPage().then(() => {
+  console.log("next step");
+});
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
@@ -17,5 +49,5 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 });
-
+*/
 // Fetch is a better way to make http requests.
